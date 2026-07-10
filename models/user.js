d8ea35992cbd0059cd644a51342c7b8e054edb53
Farhad Yousefe <farhadyousefe@gmail.com>
2026-07-10 19:34:45 +0430

    capitalize user.js to User.js and the model inside the file

--- models/user.js
+++ models/user.js
@@ -1,56 +1,54 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 import Joi from 'joi';
 
-
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        minlength: 3,
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  experiencePoints: {
+    type: Number,
+    default: 0,
+  },
+  streak: {
+    currentCount: {
+      type: Number,
+      default: 0,
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true
+    lastActiveSession: {
+      type: Date,
+      default: null,
     },
-    password: {
-        type: String,
-        required: true
-    },
-    experiencePoints: {
-        type: Number,
-        default: 0
-    },
-    streak: {
-        currentCount: {
-            type: Number,
-            default: 0
-        },
-        lastActiveSession: {
-            type: Date,
-            default: null
-        }
-    },
-    completedLessons: {
-        type: [String],
-        default: []
-    }
+  },
+  completedLessons: {
+    type: [String],
+    default: [],
+  },
 });
 
-const user = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
 function validateUser(user) {
-    const schema = Joi.object({
-        username: Joi.string().min(3).max(30).required(),
-        email: Joi.string().email().required(),
-        password: Joi.string().min(6).required()
-    });
-    return schema.validate(user)
+  const schema = Joi.object({
+    username: Joi.string().min(3).max(30).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).max(72).required(),
+  });
+  return schema.validate(user);
 }
-
 
 export { user, validateUser };
