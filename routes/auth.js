b8e0f00f2d889182bbdf23566470b049a4d5129a
Farhad Yousefe <farhadyousefe@gmail.com>
2026-07-10 19:33:55 +0430

    modified post request by adding 'register'
    improved the code and export the router

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,7 @@
 import express from 'express';
-import {user, validateUser} from "../models/user.js"
+import { User, validateUser } from '../models/User.js';
 import debug from 'debug';
 import bcrypt from 'bcrypt';
-
 
 //2. Initialize debuggers (namespaced for modular logging)
 const dbDebug = debug('app:db');
@@ -16,33 +15,36 @@ httpDebug.color = 5;
 //3. Create router instance
 const router = express.Router();
 
-router.post('/', async (req, res) => {
-    httpDebug("add/register request is recieved")
-    const {error, value} = validateUser(req.body);
-    if(error) {
-        httpDebug("error in uploaded data", error.message)
-        return res.status(400).send(`Bad Request: ${error.message}`)
-    }
-    let registeredUser = await user.findOne({email: req.body.email})
-    if(registeredUser){
-        dbDebug("user is registered!")
-        return res.status(400).send("User is registered")
-    }
-    const salt = await bcrypt.genSalt(10);
-    value.password = await bcrypt.hash(value.password, salt)
-    let newUser = new user(value)
-    try {
-        newUser = await newUser.save();
-        dbDebug('New user is saved');
+router.post('/register', async (req, res) => {
+  httpDebug('add/register request is recieved');
+  const { error, value } = validateUser(req.body);
+  if (error) {
+    httpDebug(`Joi Validation Error: ${error.details[0].message}`);
+    return res
+      .status(400)
+      .send(`Joi Validation Error: ${error.details[0].message}`);
+  }
+  let registeredUser = await User.findOne({ email: req.body.email });
+  if (registeredUser) {
+    dbDebug('user is registered!');
+    return res.status(400).send('User is registered');
+  }
+  const salt = await bcrypt.genSalt(10);
+  value.password = await bcrypt.hash(value.password, salt);
+  let newUser = new user(value);
+  try {
+    newUser = await newUser.save();
+    dbDebug('New user is saved');
 
-        const addedUser = newUser.toObject();
-        delete addedUser.password;
+    const addedUser = newUser.toObject();
+    delete addedUser.password;
 
-        httpDebug("New user is registered")
-        return res.status(201).send(addedUser)
-
-    } catch (err) {
-        dbDebug('erro in registring new user', err.message)
-        return res.status(500).send("Internal Server Error")
-    }
+    httpDebug('New user is registered');
+    return res.status(201).send(addedUser);
+  } catch (err) {
+    dbDebug('erro in registring new user', err.message);
+    return res.status(500).send('Internal Server Error');
+  }
 });
+
+export default router;
